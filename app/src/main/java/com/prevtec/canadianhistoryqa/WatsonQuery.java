662c2a15fc9397adbf609861b1393500b90b31aa
Toby Riddell <toby.riddell@prevtec.com>
2015-03-26 21:31:12 -0400

    Refined UI Created WatsonQuery class to handle interactions with Watson via REST Added default question Added dependencies and files to ignore to build.gradle

---
+++ app/src/main/java/com/prevtec/canadianhistoryqa/WatsonQuery.java
@@ -0,0 +1,17 @@
+package com.prevtec.canadianhistoryqa;
+
+/**
+ * Created by toby on 26/03/2015.
+ */
+public class WatsonQuery {
+    private String response;
+    private String question;
+
+    public String getResponse() {
+            return this.response;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
