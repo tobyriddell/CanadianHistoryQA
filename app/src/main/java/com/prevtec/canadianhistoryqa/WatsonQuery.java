c5000c4d69e6f857dda36a951c12dc8fd5d13f80
Toby Riddell <toby.riddell@prevtec.com>
2015-03-27 15:47:55 -0400

    Added REST call on Submit

--- app/src/main/java/com/prevtec/canadianhistoryqa/WatsonQuery.java
+++ app/src/main/java/com/prevtec/canadianhistoryqa/WatsonQuery.java
@@ -1,5 +1,9 @@
 package com.prevtec.canadianhistoryqa;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 /**
  * Created by toby on 26/03/2015.
  */
@@ -7,11 +11,33 @@ public class WatsonQuery {
     private String response;
     private String question;
 
+//    final HashMap<String, Object> questionObject = null;
+
+//    @SuppressWarnings("serial")
+//    HashMap<String, Object> question_info = new HashMap<String, Object>() {{
+//        put("questionText", "What is aperture?");
+//    }};
+
+
     public String getResponse() {
-            return this.response;
+           return this.response;
     }
 
     public void setQuestion(String question) {
         this.question = question;
     }
+
+//    public void setQuestion(String questionString) {
+//        HashMap<String, String> questionInner;
+//
+//        questionInner = new HashMap<String, Object>() {
+//            put("questionText", questionString);
+//        };
+//
+//        questionObject = new HashMap<String, Object>() {
+//            put("question", questionInner);
+//        };
+//
+////        this.question = question;
+//    }
 }
